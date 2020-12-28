11923736c79d78e410dc0f6e6c9f583b93f1e403
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-12-28 18:35:15 +0200

    Added texts to main activity

--- app/src/main/java/com/aarshinkov/mobile/thesheriff/MainActivity.java
+++ app/src/main/java/com/aarshinkov/mobile/thesheriff/MainActivity.java
@@ -3,6 +3,8 @@ package com.aarshinkov.mobile.thesheriff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.playersCountSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.players_count, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
